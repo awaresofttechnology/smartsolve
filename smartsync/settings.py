6859371f68d2761db265db0f288a59eaad5eb73f
Smart Solutions <sidhubrar22g@gmail.com>
2026-05-16 12:27:33 -0700

    fix: switch to CompressedStaticFilesStorage to avoid missing .map file error

--- smartsync/settings.py
+++ smartsync/settings.py
@@ -106,7 +106,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"},
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
